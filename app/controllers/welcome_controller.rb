069138dd931768131fe360dba7f5c7137f222156
Rana Roop <ranaroop@gmail.com>
2014-07-30 17:55:48 -0400

    help

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,4 @@
 class WelcomeController < ApplicationController
-  def bottles_of_beer
-  i = 99
-  while i > 0
-    puts "#{i} bottles of beer on the wall. #{i} bottle of beer."
-    i = i - 1
-    puts "Take one down, pass it around. #{i} bottle of beer on the wall."
+  def index
   end
 end
-
-bottles_of_beer
-
-puts 1+2
