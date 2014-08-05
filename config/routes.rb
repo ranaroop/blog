3bff75137764f6b7970f655a8ead16fb302f2e58
Rana Roop <ranaroop@gmail.com>
2014-08-04 20:32:38 -0400

    added root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'sitemap/index'
+
+  root 'welcome/#index'
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
---
+++ oldmother.rb
@@ -0,0 +1,2 @@
+line_width = 50
+puts (                  'old mother hubbard'.center(line_width/2))
